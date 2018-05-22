1dfb0d8a0fae90e4fa9277ebe85b6be8bf2f417e
Arjun Namineni <anamineni@gmail.com>
2018-05-22 01:37:22 -0400

    Used common variable

--- SpeedControl.js
+++ SpeedControl.js
@@ -3,15 +3,18 @@ function SetSpeed(e) {
   if (evtobj.ctrlKey)
   {
     var playbackRate = document.getElementsByTagName("video")[0].playbackRate;
-    if (evtobj.keyCode == 49) document.getElementsByTagName("video")[0].playbackRate = 0.25;
-    else if (evtobj.keyCode == 50) document.getElementsByTagName("video")[0].playbackRate = 0.5;
-    else if (evtobj.keyCode == 51) document.getElementsByTagName("video")[0].playbackRate = 1;
-    else if (evtobj.keyCode == 52) document.getElementsByTagName("video")[0].playbackRate = 1.5;
-    else if (evtobj.keyCode == 53) document.getElementsByTagName("video")[0].playbackRate = 2;
-    else if (evtobj.keyCode == 54) document.getElementsByTagName("video")[0].playbackRate = 2.5;
-    else if (evtobj.keyCode == 55) document.getElementsByTagName("video")[0].playbackRate = 3;
-    else if (evtobj.keyCode == 189) document.getElementsByTagName("video")[0].playbackRate = playbackRate - 0.5;
-    else if (evtobj.keyCode == 187) document.getElementsByTagName("video")[0].playbackRate = playbackRate + 0.5;
+    
+    if (evtobj.keyCode == 49) playbackRate = 0.25;
+    else if (evtobj.keyCode == 50) playbackRate = 0.5;
+    else if (evtobj.keyCode == 51) playbackRate = 1;
+    else if (evtobj.keyCode == 52) playbackRate = 1.5;
+    else if (evtobj.keyCode == 53) playbackRate = 2;
+    else if (evtobj.keyCode == 54) playbackRate = 2.5;
+    else if (evtobj.keyCode == 55) playbackRate = 3;
+    else if (evtobj.keyCode == 189) playbackRate -= 0.5;
+    else if (evtobj.keyCode == 187) playbackRate += 0.5;
+
+    document.getElementsByTagName("video")[0].playbackRate = playbackRate;
   }
 }
 
